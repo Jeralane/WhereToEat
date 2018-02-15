7af0150d290fd4e7ddeb3c00f0aafeaa06e45707
Jess Alejo <jess.alejo@innohub.ph>
2018-02-15 21:48:47 +0800

    Initial Commit :D

---
+++ WhereToEat/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WhereToEat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cboType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.lblFortuneMessage = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(14, 117);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(543, 340);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::WhereToEat.Properties.Resources.splash;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(541, 338);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cboType
+            // 
+            this.cboType.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboType.FormattingEnabled = true;
+            this.cboType.Location = new System.Drawing.Point(248, 81);
+            this.cboType.Margin = new System.Windows.Forms.Padding(5);
+            this.cboType.Name = "cboType";
+            this.cboType.Size = new System.Drawing.Size(308, 27);
+            this.cboType.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.Location = new System.Drawing.Point(14, 81);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 27);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ano ang feel mong kainan?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelect.Location = new System.Drawing.Point(14, 464);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(545, 50);
+            this.btnSelect.TabIndex = 4;
+            this.btnSelect.Text = "Ano ang kapalaran ng iyong sikmura at bulsa?";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.DimGray;
+            this.label2.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(15, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(541, 58);
+            this.label2.TabIndex = 5;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // lblFortuneMessage
+            // 
+            this.lblFortuneMessage.Font = new System.Drawing.Font("Tahoma", 9F);
+            this.lblFortuneMessage.Location = new System.Drawing.Point(15, 521);
+            this.lblFortuneMessage.Name = "lblFortuneMessage";
+            this.lblFortuneMessage.Size = new System.Drawing.Size(544, 26);
+            this.lblFortuneMessage.TabIndex = 6;
+            this.lblFortuneMessage.Text = "Ano pang hinihintay mo?";
+            this.lblFortuneMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 556);
+            this.Controls.Add(this.lblFortuneMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboType);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Saan Tayo Kakaen?";
+            this.TopMost = true;
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cboType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label lblFortuneMessage;
+    }
+}
+
---
+++ WhereToEat/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WhereToEat
+{
+    public partial class Form1 : Form
+    {
+        private int _counter;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeComboItems();
+            KeyDown += (sender, e) =>
+            {
+                if (e.Control && e.Shift && e.KeyCode == Keys.X)
+                {
+                    if (MessageBox.Show(@"Gusto mo bang balikan ang mga nakainan mo na?", @"Teka!", MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        Reset();
+                    }
+                }
+            };
+
+            btnSelect.Click += (s, e) =>
+            {
+                if (!ValidateFields())
+                {
+                    return;
+                }
+                Animate();
+                btnSelect.Enabled = false;
+            };
+
+            timer1.Tick += (sender, args) =>
+            {
+                _counter++;
+                var file = RandomPick(AllImage());
+                pictureBox1.Image = new Bitmap(file);
+                if (_counter >= 30)
+                {
+                    timer1.Stop();
+                    btnSelect.Enabled = true;
+                    HungerPick();
+                }
+            };
+        }
+
+        private void HungerPick()
+        {
+            var rootFolder = AppDomain.CurrentDomain.BaseDirectory;
+            var type = cboType.SelectedItem;
+
+            if (type == null)
+            {
+                return;
+            }
+
+            var imagesFolder = Path.Combine(rootFolder, type.ToString());
+
+            if (!Directory.Exists(imagesFolder)) return;
+
+            var filesFromImagesFolder = Directory.GetFiles(imagesFolder, "*.*", SearchOption.AllDirectories);
+            if (!filesFromImagesFolder.Any()) return;
+
+            var imageFiles = filesFromImagesFolder.Where(file => Regex.IsMatch(file, @".jpg|.png|.gif$")).ToList();
+            if (!imageFiles.Any()) return;
+
+            var previousFolder = Path.Combine(rootFolder, "Previous");
+            var previousCards = Directory.GetFiles(previousFolder, "*.*", SearchOption.AllDirectories);
+
+            if (previousCards.Count() == imageFiles.Count())
+            {
+                Reset();
+                previousCards = new string[] {};
+            }
+
+            var fileNames = from x in previousCards
+                select Path.GetFileName(x);
+
+            var availableCards = (from imageFile in imageFiles
+                let imageName = Path.GetFileName(imageFile)
+                where !fileNames.Contains(imageName)
+                select imageFile).ToList();
+
+            var shuffledcards = availableCards.OrderBy(a => Guid.NewGuid()).ToList();
+
+            if (!shuffledcards.Any())
+            {
+                Reset();
+                HungerPick();
+                return;
+            }
+
+            var rnd = new Random();
+            var index = rnd.Next(1, shuffledcards.Count);
+            var selectedImage = shuffledcards[index - 1];
+            if (selectedImage == null) return;
+
+            var duplicate = Path.Combine(previousFolder, Path.GetFileName(selectedImage));
+            File.Copy(selectedImage, duplicate);
+            pictureBox1.Image = new Bitmap(selectedImage);
+
+            GreetingsFor(Path.GetFileNameWithoutExtension(selectedImage));
+        }
+
+        private void Reset()
+        {
+            var rootFolder = AppDomain.CurrentDomain.BaseDirectory;
+            var previousFolder = Path.Combine(rootFolder, "Previous");
+            var di = new DirectoryInfo(previousFolder);
+
+            foreach (var file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (var dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+        }
+
+        private void InitializeComboItems()
+        {
+            cboType.Items.Clear();
+            foreach (var type in RestaurantTypes())
+            {
+                    cboType.Items.Add(type);
+            }
+        }
+
+        private List<string> RestaurantTypes()
+        {
+            var rootFolder = AppDomain.CurrentDomain.BaseDirectory;
+            var folders = Directory.GetDirectories(rootFolder);
+            return folders.Select(Path.GetFileName).Where(x => x != "Previous").ToList();
+        } 
+
+        private void Animate()
+        {
+            _counter = 0;
+            btnSelect.Enabled = false;
+            timer1.Start();
+        }
+
+        private List<string> AllImage()
+        {
+            var imageFiles = new List<string>();
+            var rootFolder = AppDomain.CurrentDomain.BaseDirectory;
+            var types = RestaurantTypes();
+            foreach (var type in types)
+            {
+                var imagesFolder = Path.Combine(rootFolder, type);
+
+                if (!Directory.Exists(imagesFolder)) continue;
+
+                var filesFromImagesFolder = Directory.GetFiles(imagesFolder, "*.*", SearchOption.AllDirectories);
+                if (!filesFromImagesFolder.Any())
+                    continue;
+                imageFiles.AddRange(
+                    filesFromImagesFolder.Where(file => Regex.IsMatch(file, @".jpg|.png|.gif$")).ToList());
+            }
+            return imageFiles;
+        }
+
+        private string RandomPick(List<string> shuffledcards)
+        {
+            var rnd = new Random();
+            var index = rnd.Next(1, shuffledcards.Count);
+            return shuffledcards[index - 1];
+        }
+
+        private bool ValidateFields()
+        {
+            var type = cboType.SelectedItem;
+            if (type == null)
+            {
+                MessageBox.Show(@"Mamili ka muna kung ano ang tipo ng kainan na gusto mong puntahan! Excited?", @"Teka!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+            return true;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label2.Text = LocalDateTime(DateTime.Now);
+        }
+
+        private List<string> Expressions()
+        {
+            return new List<string>
+            {
+                "matutuwa",
+                "masisiyahan",
+                "maiiyak",
+                "malulungkot",
+                "maiinis",
+                "mababaliw",
+                "mabubusog",
+                "lalong magugutom",
+                "matatawa",
+                "mababaliw",
+                "masasarapan",
+                "maaaliw",
+                "malilibang",
+                "makakatagpo ng swerte",
+                "maglalabas ng sikreto",
+                "maglalabas ng sama ng loob",
+                "manlilibre",
+                "mag-eextra rice",
+                "makakahanap ng pag-ibig",
+            };
+        }
+
+        private void GreetingsFor(string restaurant)
+        {
+            var message = string.Format("Ikaw ay {0} sa iyong pagkain sa {1}!", RandomPick(Expressions()), restaurant);
+            lblFortuneMessage.Text = message;
+        }
+
+        private string LocalDateTime(DateTime dateTime)
+        {
+            var x = string.Format("{0:F}", dateTime);
+
+            var buwan = MonthLookup()[dateTime.Month];
+            var araw = DayLookup()[dateTime.DayOfWeek];
+
+            var time = dateTime.ToString("hh:mm:ss tt").Replace("AM", "NU").Replace("PM", "NH");
+
+            return string.Format("{0}, {1} {2}, {3} {4}", araw, buwan, dateTime.Day, dateTime.Year, time);
+        }
+
+        private Dictionary<int, string> MonthLookup()
+        {
+            var lookup = new Dictionary<int, string>
+            {
+                {1, "Enero"},
+                {2, "Pebrero"},
+                {3, "Marso"},
+                {4, "Abril"},
+                {5, "Mayo"},
+                {6, "Hunyo"},
+                {7, "Hulyo"},
+                {8, "Agosto"},
+                {9, "Setyembre"},
+                {10, "Oktubre"},
+                {11, "Nobyembre"},
+                {12, "Disyembre"}
+            };
+
+            return lookup;
+        }
+
+        private Dictionary<DayOfWeek, string> DayLookup()
+        {
+            var lookup = new Dictionary<DayOfWeek, string>();
+            lookup.Add(DayOfWeek.Monday, "Lunes");
+            lookup.Add(DayOfWeek.Tuesday, "Martes");
+            lookup.Add(DayOfWeek.Wednesday, "Miyerkules");
+            lookup.Add(DayOfWeek.Thursday, "Huwebes");
+            lookup.Add(DayOfWeek.Friday, "Biyernes");
+            lookup.Add(DayOfWeek.Saturday, "Sabado");
+            lookup.Add(DayOfWeek.Sunday, "Linggo");
+            return lookup;
+        }
+    }
+}
